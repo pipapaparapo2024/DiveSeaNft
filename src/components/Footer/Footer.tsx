e25567b74a4af07301f6038a24ad02cff3ceef51
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2026-02-06 00:05:20 +0300

    1300px long

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Footer.module.scss';
 
 export default function Footer() {
@@ -6,11 +7,7 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.content}>
         <div className={styles.logo}>
-          <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-             <path d="M2 12C2 12 5 8 12 8C19 8 22 12 22 12" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-             <path d="M2 16C2 16 5 12 12 12C19 12 22 16 22 16" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-             <path d="M2 8C2 8 5 4 12 4C19 4 22 8 22 8" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-          </svg>
+          <Image src="/Logo.svg" alt="DiveSea" width={30} height={30} />
           <span>DiveSea</span>
         </div>
         <div className={styles.links}>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Header.module.scss';
 import classNames from 'classnames';
 
@@ -29,29 +30,34 @@ export default function Header() {
 
   return (
     <header className={classNames(styles.header, { [styles.scrolled]: scrolled })}>
-      <div className={styles.logo}>
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M2 12C5 8 9 8 12 12C15 16 19 16 22 12" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M2 16C5 12 9 12 12 16C15 20 19 20 22 16" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-      </div>
-      
-      {/* Desktop Nav */}
-      <nav className={styles.nav}>
-        <Link href="#">Discover</Link>
-        <Link href="#">Creators</Link>
-        <Link href="#">Sell</Link>
-        <Link href="#">Stats</Link>
-      </nav>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image 
+            src="/Logo.svg" 
+            alt="DiveSea" 
+            width={40} 
+            height={40} 
+            priority
+          />
+        </div>
+        
+        {/* Desktop Nav */}
+        <nav className={styles.nav}>
+          <Link href="#">Discover</Link>
+          <Link href="#">Creators</Link>
+          <Link href="#">Sell</Link>
+          <Link href="#">Stats</Link>
+        </nav>
 
-      {/* Hamburger */}
-      <div 
-        className={classNames(styles.burger, { [styles.open]: menuOpen })}
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
+        {/* Hamburger */}
+        <div 
+          className={classNames(styles.burger, { [styles.open]: menuOpen })}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
 
       {/* Mobile Menu */}
